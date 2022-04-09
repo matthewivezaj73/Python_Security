ddb2b2259875c9b8677d1a3fa7c327589b82d445
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-04-09 16:35:53 -0400

    Added a function.

--- syn_scanner.py
+++ syn_scanner.py
@@ -5,4 +5,7 @@ import ipaddress
 #Creating a list of ports to scan.
 ports = [25, 80, 53, 443, 445, 8080, 8443]
 #Begin creating a function.
-def SynScan(host)
+def SynScan(host):
+    """
+    A function that will carry out a Syn scan.
+    """
