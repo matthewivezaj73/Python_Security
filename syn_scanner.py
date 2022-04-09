9df8134371a096572fb38f56653370fdc46fe602
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-04-09 16:51:19 -0400

    Finished crating SynScan function.

--- syn_scanner.py
+++ syn_scanner.py
@@ -10,5 +10,10 @@ def SynScan(host):
     A function that will carry out a Syn scan.
     """
     ans, unans = sr(
-        IP(dst=host)
-    )
+        IP(dst=host)/
+        TCP(sport=33333,dport=ports,flags="S")
+        ,timeout=2,verbose=0)
+    print("Open ports at %s:" % host)
+    for (s,r,) in ans:
+        if s[TCP].dport == r[TCP].sport and r[TCP].flags=="SA":
+            print(s[TCP].dport)
