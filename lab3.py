7568a89ed0d9d9a2cccb127154295a0d024622ed
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-04-10 08:15:00 -0400

    entered a path variable for cd desktop.

--- lab3.py
+++ lab3.py
@@ -1,10 +1,13 @@
 #importing libraries.
 import os
-#Changing directories to users.
-#Executing dir command.
-os.system('dir')
 #Asking for user input.
 enter_username = input("Please enter your username: ")
+my_path = f"C:/Users{enter_username}/Destop"
+#Asking for user input.
+enter_username = input("Please enter your username: ")
+#Executing dir command.
+os.system('dir')
+
 #Executing dir command.
 os.system('dir')
 #Asking for the path.
