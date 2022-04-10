4d35b7e1142f7dce8c128d71362b03e76f360c7f
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-04-09 22:05:49 -0400

    added code to run netstat.

--- windows_commands_security.py
+++ windows_commands_security.py
@@ -1,3 +1,11 @@
 #Importing subprocess library.
 import subprocess
-subprocess.run(["ls", "-la"])
+import sys
+output = subprocess.Popen(['netstat', '-ano'], stdout=subprocess.PIPE).communicate()
+
+newlines = str(output[0]).split('\r\n')  
+for line in newlines:
+    if line.find("Foreign") != -1:
+        print(line)
+    if line.find("ESTABLISHED") != -1:
+        print(line)
