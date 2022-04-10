02a89635a6843639c1983bc3a12620fdc8ba1b72
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-04-09 22:07:56 -0400

    Added a comment.

--- windows_commands_security.py
+++ windows_commands_security.py
@@ -3,8 +3,9 @@ import subprocess
 import sys
 #Assigning a variable the output of netstat -ano.
 output = subprocess.Popen(['netstat', '-ano'], stdout=subprocess.PIPE).communicate()
-
+#Reading index 0 and inserting a newline char plus a carraige return.
 newlines = str(output[0]).split('\r\n')  
+#Running through each line in the newlines variable and printing based off of certain criteria.
 for line in newlines:
     if line.find("Foreign") != -1:
         print(line)
