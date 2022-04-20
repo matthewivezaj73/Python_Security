9b809bb9fddc100c455f70ff61237672465fc0a3
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-04-19 20:31:37 -0400

    Delete ch2 directory

--- ch2/basic_pefile.py
+++
@@ -1,7 +0,0 @@
-#Import libraries
-import pefile
-pe = pefile.PE("/home/monty/Desktop/malware_data_science_entrypoints_redacted/malware_data_science/ch2/ircbot.exe")
-#Parsing the executable.
-for section in pe.sections:
-    malicious_output = section.Name,hex(section.VirtualAddress), hex(section.Misc_VirtualSize), section.SizeOfRawData
-    print(malicious_output)
